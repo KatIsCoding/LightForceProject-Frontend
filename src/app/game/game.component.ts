033f666e76bf415d6897271022df24a7ae8d189d
KatIsCoding <fabrigoru@gmail.com>
2023-02-15 01:12:29 -0600

    Setup main game logic

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
+enum PossibleDirections {
+  Up = 'ArrowUp',
+  Down = 'ArrowDown',
+  Left = 'ArrowLeft',
+  Right = 'ArrowRight',
+}
+
+const possibleRotationAngles = [45, 135, -45, -135];
 
 @Component({
   selector: 'app-game',
@@ -6,6 +15,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./game.component.css'],
 })
 export class GameComponent {
-  direction = 'Left';
-  rotationAngle = '135deg';
+  direction: keyof typeof PossibleDirections = 'Down';
+  rotationAngle = '-45deg';
+  running = true;
+  score = 0;
+
+  private setNextDirection() {
+    const randomDirection = <keyof typeof PossibleDirections>(
+      Object.keys(PossibleDirections)[
+        Math.floor(Math.random() * Object.keys(PossibleDirections).length)
+      ]
+    );
+    console.log(randomDirection);
+
+    const randomRotationAngle =
+      possibleRotationAngles[
+        Math.floor(Math.random() * possibleRotationAngles.length)
+      ] + 'deg';
+
+    this.direction = randomDirection;
+
+    this.rotationAngle = randomRotationAngle;
+  }
+
+  private checkKeyboardInput(event: KeyboardEvent) {
+    if (event.code === PossibleDirections[this.direction]) {
+      this.score++;
+    }
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (!this.running) {
+      return;
+    }
+
+    this.checkKeyboardInput(event);
+    this.setNextDirection();
+  }
 }
