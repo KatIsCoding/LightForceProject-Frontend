a8d6e8da484cf875c2b373076b7efcf72f32553d
KatIsCoding <fabrigoru@gmail.com>
2023-02-15 04:24:36 -0600

    Add ledger component

--- src/app/ledger/ledger.component.ts
+++ src/app/ledger/ledger.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CloudService } from './../cloud.service';
 
 @Component({
   selector: 'app-ledger',
   templateUrl: './ledger.component.html',
-  styleUrls: ['./ledger.component.css']
+  styleUrls: ['./ledger.component.css'],
 })
 export class LedgerComponent {
-
+  constructor(public cloudService: CloudService) {}
 }
