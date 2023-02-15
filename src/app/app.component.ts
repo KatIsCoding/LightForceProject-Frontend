478cc39a11b33f0c2203817fc29e054727ea5bb1
KatIsCoding <fabrigoru@gmail.com>
2023-02-14 22:11:54 -0600

    Consume CloudService in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CloudService } from './cloud.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'lightforce-arcade';
+  title = 'LightForce Arcade';
+
+  constructor(public cloudService: CloudService) {}
 }
