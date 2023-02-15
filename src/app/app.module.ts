a9383572384f12ffde8d93c6a30a3cd6f3abaccf
KatIsCoding <fabrigoru@gmail.com>
2023-02-15 17:29:27 -0600

    Implement http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
@@ -37,6 +39,7 @@ import { MatListModule } from '@angular/material/list';
     MatDialogModule,
     MatFormFieldModule,
     MatListModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
