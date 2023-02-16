7fb51db6649a5908a1006015c5c19096aab7ea73
KatIsCoding <fabrigoru@gmail.com>
2023-02-16 04:25:29 -0600

    Introduce ledge interface

--- src/app/user.data.ts
+++ src/app/user.data.ts
@@ -1,4 +1,11 @@
+export interface Ledge {
+  description: string;
+  debit: number;
+  credit: number;
+}
+
 export interface UserData {
   username: string;
   tokens: number;
+  ledge: Ledge[];
 }
